9baf865b1c60850bc85af5c75b63cb4caf095c23
Alexander Smirnov <al.smirnov996@gmail.com>
2018-04-20 22:28:45 +0300

    Create user model

---
+++ users/models.py
@@ -0,0 +1,14 @@
+import sqlalchemy as sa
+
+user_meta = sa.MetaData()
+
+user = sa.Table(
+    'user', user_meta,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('username', sa.String(200), nullable=False, unique=True),
+    sa.Column('password', sa.String(200), nullable=False),
+    sa.Column('first_name', sa.String(200)),
+    sa.Column('last_name', sa.String(200)),
+    sa.Column('role', sa.Integer, nullable=False),
+    sa.Column('blocked', sa.Boolean, nullable=False, default=False)
+)
