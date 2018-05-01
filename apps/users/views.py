f8d8b784c6c2f72dc93c6b1a6633f4d18bc2829a
Alexander Smirnov <al.smirnov996@gmail.com>
2018-05-01 17:06:19 +0300

    Update routes and add block/unblock user methods

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,14 @@
+from aiohttp import web
+
 from apps.users.models import User
 
 from apps.users.serializers import UserSerializer
 from utils import views
 
+user_routes = web.RouteTableDef()
 
+
+@user_routes.view('/api/users/')
 class UsersView(views.ListView):
     model = User
     serializer_class = UserSerializer
@@ -16,6 +21,26 @@ class UsersView(views.ListView):
             return User.c.blocked == False
 
 
+@user_routes.view(r'/api/users/{pk:\d+}/')
 class UserView(views.DetailView):
     model = User
     serializer_class = UserSerializer
+
+
+@user_routes.post(r'/api/users/{pk:\d+}/block/')
+async def block_user(request):
+    await change_user_status(request, blocked=True)
+    return web.Response()
+
+
+@user_routes.post(r'/api/users/{pk:\d+}/unblock/')
+async def unblock_user(request):
+    await change_user_status(request, blocked=False)
+    return web.Response()
+
+
+async def change_user_status(request, blocked=False):
+    async with request.app['db'].acquire() as conn:
+        pk = request.match_info['pk']
+        query = User.update().where(User.c.id == pk).values(blocked=blocked)
+        await conn.execute(query)
--- project/routes.py
+++ project/routes.py
@@ -1,12 +1,12 @@
 from aiohttp import web
 
-from apps.users.views import UsersView, UserView
+from apps.users.views import user_routes
 from utils.auth_token.views import AuthTokenView
 
 
 def setup_routes(app):
     app.add_routes([
-        web.view('/api/login/', AuthTokenView),
-        web.view('/api/users/', UsersView),
-        web.view(r'/api/users/{pk:\d+}/', UserView),
+        web.view('/api/login/', AuthTokenView)
     ])
+
+    app.add_routes(user_routes)
