e6f0852453c3a08cb25e9d5bea041d58affb0f31
Alexander Smirnov <al.smirnov996@gmail.com>
2018-05-03 18:41:12 +0300

    Add role constants

--- project/permissions.py
+++ project/permissions.py
@@ -1,8 +1,13 @@
 from utils.permissions import IsAuthenticated
 
+STUDENT = 1
+TEACHER = 2
+MODERATOR = 3
+ADMIN = 4
+
 
 class IsAdmin(IsAuthenticated):
     async def has_permission(self, request):
         await super(IsAdmin, self).has_permission(request)
         user = request['user']
-        return user.role == 3
+        return user.role == ADMIN
