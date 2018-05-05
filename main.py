3e3e2fd54c2bae9509868851656a5dd5b7a1ade4
Alexander Smirnov <al.smirnov996@gmail.com>
2018-05-05 20:02:09 +0300

    Add logging

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import logging
+
 from aiohttp import web
 
 from project import settings
 from project.configuration import configure_app
 
+logging.basicConfig(level=logging.INFO)
+
+
 if __name__ == '__main__':
+
     app = web.Application()
     configure_app(app)
     web.run_app(app, host=settings.HOST, port=settings.PORT)
