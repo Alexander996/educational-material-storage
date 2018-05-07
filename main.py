2d8ce43ca8b340a0b61413a289d825c7e27f5d8d
Alexander Smirnov <al.smirnov996@gmail.com>
2018-05-07 18:05:17 +0300

    Update starting server

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ from project.configuration import configure_app
 
 logging.basicConfig(level=logging.INFO)
 
-parser = argparse.ArgumentParser(description="aiohttp server example")
-parser.add_argument('--path')
+parser = argparse.ArgumentParser(description="Educational material storage server")
+parser.add_argument('--path', help='Path to unix socket')
 parser.add_argument('--port')
+parser.add_argument('--host')
 
 
 if __name__ == '__main__':
 
     app = web.Application()
     configure_app(app)
+
     args = parser.parse_args()
-    web.run_app(app, path=args.path, port=args.port)
+    path = args.path
+    host = args.host
+    port = args.port
+
+    if path is None:
+        if host is None:
+            host = settings.HOST
+        if port is None:
+            port = settings.PORT
+
+    web.run_app(app, path=path, host=host, port=port)
