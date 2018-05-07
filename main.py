b21727743b0aca07799757e590967b0060dd83dc
Alexander Smirnov <al.smirnov996@gmail.com>
2018-05-07 15:23:37 +0300

    Deployment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 from aiohttp import web
@@ -7,9 +8,14 @@ from project.configuration import configure_app
 
 logging.basicConfig(level=logging.INFO)
 
+parser = argparse.ArgumentParser(description="aiohttp server example")
+parser.add_argument('--path')
+parser.add_argument('--port')
+
 
 if __name__ == '__main__':
 
     app = web.Application()
     configure_app(app)
-    web.run_app(app, host=settings.HOST, port=settings.PORT)
+    args = parser.parse_args()
+    web.run_app(app, path=args.path, port=args.port)
